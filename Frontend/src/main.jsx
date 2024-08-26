c7c0efbb6c437e75935f46062c06b4ab3280c47b
Shinzo27 <20bmiit031@gmail.com>
2024-08-26 22:14:53 +0530

    frontend

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,16 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'  
 
-export const Context = createContext({isAuthenticated: false})
-
 const AppWrapper = () =>{
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [user, setUser] = useState({})
-
   return (
-    <Context.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}> 
       <App/>
-    </Context.Provider> 
   )
 }
 
